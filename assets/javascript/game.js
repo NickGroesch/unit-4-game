418269e1042aed06858dad5b0764cff8db3df106
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-03-05 16:55:28 -0600

    crystals, win, lose functionality working; need to do reset logic, perhaps switch alerts to dynamic feedback

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
-  var total = 0;
-  var target = 50;
+  var total;
+  var target;
+  function reset() {
+    total = 0;
+    target = Math.ceil(Math.random() * 102) + 18;
+    var c0 = Math.ceil(Math.random() * 12);
+    $("#c0").attr("data-points", c0);
+    var c1 = Math.ceil(Math.random() * 12);
+    $("#c1").attr("data-points", c1);
+    var c2 = Math.ceil(Math.random() * 12);
+    $("#c2").attr("data-points", c2);
+    var c3 = Math.ceil(Math.random() * 12);
+    $("#c3").attr("data-points", c3);
+  }
+
+  reset();
   $("#target").text(target);
+  $("#score").text(total);
+
   $(".crystalPic").on("click", function() {
-    total += 5;
-    console.log("you clicked crystal", this.alt, ";", total);
+    morePoints = parseInt($(this).attr("data-points"));
+    total += morePoints;
+    $("#score").text(total);
+    console.log("you clicked crystal", this.id, ";", total);
+    if (total == target) {
+      alert("you win");
+    } else if (total >= target) {
+      alert("you lose");
+    }
   });
 });
